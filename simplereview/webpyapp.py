5cd575de4f15edec89360ed81c9fd37ea4db741e
Rickard Lindberg <ricli85@gmail.com>
2011-08-21 17:18:48 +0200

    Add review page.

--- simplereview/webpyapp.py
+++ simplereview/webpyapp.py
@@ -10,6 +10,7 @@ TEMPLATE_DIR = os.getenv("TEMPLATE_DIR", "templates/")
 URLS = (
     "/", "list_reviews",
     "/create_review", "create_review",
+    "/review/(.*)", "review",
 )
 
 render = web.template.render(TEMPLATE_DIR, base="base")
@@ -18,6 +19,14 @@ repo = SqliteReviewRepository(DB_PATH)
 class list_reviews:
     def GET(self):
         return render.list_reviews(repo.list_by_date())
+
+class review:
+    def GET(self, id_):
+        r = None
+        for review in repo.list_by_date():
+            if str(review.id_) == str(id_):
+                r = review
+        return render.review(r)
 
 class create_review:
     def POST(self):
--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -28,9 +28,13 @@ class AcceptanceTest(unittest.TestCase):
     def kill_server(self):
         self.server_process.kill()
 
-    def test_foo(self):
+    def test_has_review_on_list_page(self):
         self.add_review_with_name("review 1")
-        self.assert_list_page_contains("review 1")
+        self.assert_page_contains("/", "review 1")
+
+    def test_has_review_on_review_page(self):
+        self.add_review_with_name("review 2")
+        self.assert_page_contains("/review/1", "review 2")
 
     def add_review_with_name(self, name):
         params = urllib.urlencode({
@@ -38,8 +42,8 @@ class AcceptanceTest(unittest.TestCase):
         })
         urllib.urlopen("http://localhost:8080/create_review", params)
 
-    def assert_list_page_contains(self, part):
-        content = self.read_content_from("/")
+    def assert_page_contains(self, page, part):
+        content = self.read_content_from(page)
         if part not in content:
             self.fail("Did not find '%s' in '%s'" % (part, content))
         

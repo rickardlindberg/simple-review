113194046c7f8193d18c2d3b1bdf0523f598f5c1
Rickard Lindberg <ricli85@gmail.com>
2011-08-21 16:47:15 +0200

    Implement basic web app.

---
+++ simplereview/webpyapp.py
@@ -0,0 +1,28 @@
+import os
+
+from simplereview.domain import Review
+from simplereview.repositories import SqliteReviewRepository
+
+import web
+
+DB_PATH = os.getenv("DB_PATH", "db.db")
+TEMPLATE_DIR = os.getenv("TEMPLATE_DIR", "templates/")
+URLS = (
+    "/", "list_reviews",
+    "/create_review", "create_review",
+)
+
+render = web.template.render(TEMPLATE_DIR, base="base")
+repo = SqliteReviewRepository(DB_PATH)
+
+class list_reviews:
+    def GET(self):
+        return render.list_reviews(repo.list_by_date())
+
+class create_review:
+    def POST(self):
+        i = web.webapi.input()
+        repo.save(Review(name=i.name))
+
+if __name__ == "__main__":
+    web.application(URLS, globals()).run()
---
+++ tests/test_acceptance.py
@@ -0,0 +1,47 @@
+import os.path
+import shutil
+import subprocess
+import tempfile
+import time
+import unittest
+import urllib
+
+
+class AcceptanceTest(unittest.TestCase):
+
+    def setUp(self):
+        self.tmp_dir = tempfile.mkdtemp("simplereview")
+        self.start_server()
+        self.wait_for_server_to_start()
+
+    def tearDown(self):
+        self.kill_server()
+        shutil.rmtree(self.tmp_dir)
+
+    def start_server(self):
+        os.putenv("DB_PATH", os.path.join(self.tmp_dir, "foo.db"))
+        self.server_process = subprocess.Popen(["python", "simplereview/webpyapp.py"])
+
+    def wait_for_server_to_start(self):
+        time.sleep(1)
+
+    def kill_server(self):
+        self.server_process.kill()
+
+    def test_foo(self):
+        self.add_review_with_name("review 1")
+        self.assert_list_page_contains("review 1")
+
+    def add_review_with_name(self, name):
+        params = urllib.urlencode({
+            "name": name
+        })
+        urllib.urlopen("http://localhost:8080/create_review", params)
+
+    def assert_list_page_contains(self, part):
+        content = self.read_content_from("/")
+        if part not in content:
+            self.fail("Did not find '%s' in '%s'" % (part, content))
+        
+    def read_content_from(self, path):
+        return urllib.urlopen("http://localhost:8080%s" % path).read()

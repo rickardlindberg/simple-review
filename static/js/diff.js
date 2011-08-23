e39049d9e21a4e074ee50ce8af88567e591de2ec
Rickard Lindberg <ricli85@gmail.com>
2011-08-23 20:01:55 +0200

    Clean up js code.

--- static/js/diff.js
+++ static/js/diff.js
@@ -1,68 +1,45 @@
-function diff(element, id) {
-    new Ajax.Request('/review_diff/' + id, {
-        method: "get",
-        onSuccess: function (transport) {
-            var response = transport.responseText || "{}";
-            var json = response.evalJSON();
-            output(element, json);
-        },
-        onFailure: function () {
-            alert('Something went wrong...')
-        }
-    });
-}
+var DiffLoader = {
 
-function output(element, json) {
-    json.each(function (file) {
-        element.appendChild(file_to_html(file));
-    });
-}
-
-function file_to_html(file) {
-    var file_div = div("file");
-
-    header = div("file-header");
-    header.appendChild(span(file.old));
-    file_div.appendChild(header);
-
-    file.hunks.each(function (hunk) {
-        hunk_to_html(file_div, hunk);
-    });
-    return file_div;
-}
-
-function hunk_to_html(file_div, hunk) {
-    file_div.appendChild(line_to_html(hunk.line, "hunk"));
-    hunk.parts.each(function (part) {
-        part.lines.each(function (line) {
-            file_div.appendChild(line_to_html(line, part.type));
+    load: function (review_id, containerElement) {
+        $.getJSON("/review_diff/" + review_id, function (diffJson) {
+            DiffDomBuilder.build(diffJson).appendTo(containerElement);
         });
-    });
-}
+    }
 
-function line_to_html(line, type) {
-    var line_div = div("line");
-    var margin_div = div("margin");
-    var content_div = div("content");
+};
 
-    margin_div.appendChild(span(line.number));
+var DiffDomBuilder = {
 
-    Element.addClassName(content_div, type);
-    content_div.appendChild(span(line.content));
+    build: function (diffJson) {
+        var diffDiv = $("<div/>").addClass("diff");
+        DiffDomBuilder._buildFiles(diffDiv, diffJson);
+        return diffDiv;
+    },
 
-    line_div.appendChild(margin_div);
-    line_div.appendChild(content_div);
-    return line_div;
-}
+    _buildFiles: function (diffDiv, diffedFiles) {
+        $.each(diffedFiles, function (i, diffedFile) {
+            var fileDiv = $("<div/>").addClass("file");
+            fileDiv.append($("<div/>").addClass("file-header").text(diffedFile.old));
+            DiffDomBuilder._buildLines(fileDiv, diffedFile);
+            diffDiv.append(fileDiv);
+        });
+    },
 
-function span(text) {
-    var span = document.createElement("pre");
-    span.innerHTML = text;
-    return span;
-}
+    _buildLines: function (fileDiv, diffedFile) {
+        $.each(diffedFile.hunks, function (i, hunk) {
+            DiffDomBuilder._buildLine("hunk", hunk.line).appendTo(fileDiv);
+            $.each(hunk.parts, function (i, part) {
+                $.each(part.lines, function (i, line) {
+                    DiffDomBuilder._buildLine(part.type, line).appendTo(fileDiv);
+                });
+            });
+        });
+    },
 
-function div(className) {
-    var div = document.createElement("div");
-    Element.addClassName(div, className);
-    return div;
-}
+    _buildLine: function (type, jsonLine) {
+        return $("<div/>").addClass("line-" + type).append(
+            $("<div/>").addClass("margin").text(jsonLine.number),
+            $("<pre/>").text(jsonLine.content));
+    }
+
+};

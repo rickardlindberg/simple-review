6eacd14d5b76570017ef430f56dbd6e6c2dce32e
Rickard Lindberg <ricli85@gmail.com>
2011-08-26 17:15:22 +0200

    Clean up web app.

--- simplereview.py
+++ simplereview.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 
 from simplereview.diffparser import parse
@@ -6,9 +8,9 @@ from simplereview.repositories import SqliteReviewRepository
 
 import web
 
-DB_PATH = os.getenv("DB_PATH", "dev.db")
-TEMPLATE_DIR = os.getenv("TEMPLATE_DIR", "templates/")
-URLS = (
+repo = SqliteReviewRepository(os.getenv("DB_PATH", "dev.db"))
+render = web.template.render(os.getenv("TEMPLATE_DIR", "templates/"), base="base")
+urls = (
     "/", "list_reviews",
     "/create_review", "create_review",
     "/review/(.*)/comments_json", "comments_json",
@@ -17,26 +19,19 @@ URLS = (
     "/review_diff/(.*)", "review_diff",
 )
 
-render = web.template.render(TEMPLATE_DIR, base="base")
-repo = SqliteReviewRepository(DB_PATH)
-
 class list_reviews:
     def GET(self):
         return render.list_reviews(repo.list_by_date())
 
-class review:
-    def GET(self, id_):
-        return render.review(repo.find_by_id(id_))
-
-class review_diff:
-    def GET(self, id_):
-        web.header("Content-Type", "application/json")
-        return parse(repo.find_by_id(id_).diff).to_json()
+class create_review:
+    def POST(self):
+        i = web.webapi.input()
+        repo.save(Review(name=i.name))
 
 class comments_json:
-    def GET(self, id_):
+    def GET(self, review_id):
         web.header("Content-Type", "application/json")
-        return repo.find_by_id(id_).comments_json()
+        return repo.find_by_id(review_id).comments_json()
 
 class add_comment:
     def POST(self, review_id):
@@ -44,10 +39,14 @@ class add_comment:
         repo.add_comment(review_id, i.author, i.comment)
         web.seeother("/review/%s" % review_id)
 
-class create_review:
-    def POST(self):
-        i = web.webapi.input()
-        repo.save(Review(name=i.name))
+class review:
+    def GET(self, review_id):
+        return render.review(repo.find_by_id(review_id))
+
+class review_diff:
+    def GET(self, review_id):
+        web.header("Content-Type", "application/json")
+        return parse(repo.find_by_id(review_id).diff).to_json()
 
 if __name__ == "__main__":
-    web.application(URLS, globals()).run()
+    web.application(urls, globals()).run()

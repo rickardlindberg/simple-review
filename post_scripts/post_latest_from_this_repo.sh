44bf4134473199f97c69d1815f4c8ca0de729911
Rickard Lindberg <ricli85@gmail.com>
2011-08-27 08:57:37 +0200

    Refactor post script.

--- post_scripts/post_latest_from_this_repo.sh
+++ post_scripts/post_latest_from_this_repo.sh
@@ -1,21 +1,33 @@
 #!/bin/sh
 
-NUM=5
-FILE=tmp.diff
+SERVER="http://localhost:8080"
+NUM_REVS_TO_INCLUDE=5
+DIFF_FILE_NAME=tmp.diff
+
+post_latest() {
+    for rev in $(latest_revisions); do
+        extract_review_data
+        post_review
+    done
+    rm $DIFF_FILE_NAME
+}
+
+latest_revisions() {
+    git log -${NUM_REVS_TO_INCLUDE} | grep ^commit | awk '{ print $2 }'
+}
+
+extract_review_data() {
+    git diff ${rev}^ $rev > $DIFF_FILE_NAME
+    message=$(git show -s --format=format:%s ${rev}^{commit})
+    author=$(git show -s --format=format:%cn ${rev}^{commit})
+}
 
 post_review() {
     curl \
         --data-urlencode "name=${message}" \
         --data-urlencode "user=${author}" \
-        --data-urlencode "diff@${FILE}" \
-        http://localhost:8080/create_review
+        --data-urlencode "diff@${DIFF_FILE_NAME}" \
+        ${SERVER}/create_review
 }
 
-for rev in $(git log -$NUM | grep ^commit | awk '{ print $2 }'); do
-    git diff ${rev}^ $rev > $FILE
-    message=$(git show -s --format=format:%s ${rev}^{commit})
-    author=$(git show -s --format=format:%cn ${rev}^{commit})
-    post_review
-done;
-
-rm $FILE
+post_latest

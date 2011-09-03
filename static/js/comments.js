3d5896fe86f1bb60eed7af09dc110a662d49a7d6
Rickard Lindberg <ricli85@gmail.com>
2011-09-03 16:33:32 +0200

    Display diff using table and position popups as absoulte instead of fixed.

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,55 +1,88 @@
-function loadCommentsFor(review_id) {
-    $.getJSON("/review/" + review_id + "/comments_json", function (commentsJson) {
+function initComments(reviewId) {
+    populateAuthorFiledInFormFromCookie();
+    saveAuthorToCookieWhenSubmittingForm();
+    showAddCommentImageWhenHoveringLine();
+    showLineCommentsWhenHoveringCommentImage();
+    showAddLineCommentFormWhenClickingAddCommentIcon();
+    loadCommentsFor(reviewId);
+}
+
+function populateAuthorFiledInFormFromCookie() {
+    $("input[name=author]").val($.cookie("commentAuthor"));
+}
+
+function saveAuthorToCookieWhenSubmittingForm() {
+    $(".addCommentForm").submit(function () {
+        var author = $(this).find("input[name=author]").val();
+        $.cookie("commentAuthor", author, { expires: 100 });
+    });
+}
+
+function showAddCommentImageWhenHoveringLine() {
+    $(".line").hover(
+        function () {
+            $(this).find(".add-comment").show();
+        },
+        function () {
+            $(this).find(".add-comment").hide();
+        }
+    );
+}
+
+function showLineCommentsWhenHoveringCommentImage() {
+    $(".has-comment").hover(
+        function () {
+            var pos = $(this).position();
+            var lineComments = $(this).parent().find(".line-comments");
+            showElementAtAbsolutePosition(lineComments, pos.left, pos.top+20);
+        },
+        function () {
+            $(this).parent().find(".line-comments").hide();
+        }
+    );
+}
+
+function showAddLineCommentFormWhenClickingAddCommentIcon() {
+    $(".add-comment").click(
+        function (event) {
+            var id = $(this).attr("id");
+            var line = id.substring(id.lastIndexOf("-") + 1);
+            var pos = $(this).position();
+            $("#linenum").val(line);
+            showElementAtAbsolutePosition($("#line-comment-form"), pos.left+20, pos.top+20);
+            event.preventDefault();
+        }
+    );
+}
+
+function showElementAtAbsolutePosition(element, left, top) {
+    element.css("left", left);
+    element.css("top", top);
+    element.show();
+}
+
+function loadCommentsFor(reviewId) {
+    $.getJSON("/review/" + reviewId + "/comments_json", function (commentsJson) {
         for (var line in commentsJson) {
             if (line !== "-1") {
-                $("#line-margin-" + line + " .has-comment").show();
-
-                var commentsDiv = $("<div/>").addClass("line-comments");
-                $.each(commentsJson[line], function (i, comment) {
-                    if (i !== 0) {
-                        commentsDiv.append($("<hr/>"));
-                    }
-                    commentsDiv.append($("<div/>").addClass("line-comment").html(
-                        "<i>by <b>" + comment.author + "</b> at " + comment.date + "</i><br />" +
-                        comment.text
-                    ));
-                });
-                $("#line-margin-" + line).append(commentsDiv);
+                var hasCommentImage = $(".line-number-" + line + " .has-comment");
+                hasCommentImage.show();
+                hasCommentImage.parent().append(createCommentsBox(commentsJson[line]));
             }
         }
     });
 }
 
-function registerMarginHoverHandlers() {
-    $(".margin").hover(
-        function () {
-            $(this).find(".add-comment").show();
-
-            var pos = $(this).find(".has-comment").offset();
-            pos.top += 20;
-            $(this).find(".line-comments").offset(pos);
-            $(this).find(".line-comments").show();
-        },
-        function () {
-            $(this).find(".add-comment").hide();
-
-            $(this).find(".line-comments").hide();
+function createCommentsBox(comments) {
+    var commentsDiv = $("<div/>").addClass("line-comments");
+    $.each(comments, function (i, comment) {
+        if (i !== 0) {
+            commentsDiv.append($("<hr/>"));
         }
-    );
-}
-
-function registerAddLineCommentClickHandlers() {
-    $(".add-comment").click(
-        function (event) {
-            var id = $(this).parent().parent().attr("id");
-            var line = id.substring(id.lastIndexOf("-") + 1);
-            var pos = $(this).offset();
-            pos.top += 20;
-            pos.left += 20;
-            $("#linenum").val(line);
-            $("#line-comment-form").offset(pos);
-            $("#line-comment-form").show();
-            event.preventDefault();
-        }
-    );
+        commentsDiv.append($("<div/>").addClass("line-comment").html(
+            "<i>by <b>" + comment.author + "</b> at " + comment.date + "</i><br />" +
+            comment.text
+        ));
+    });
+    return commentsDiv;
 }
